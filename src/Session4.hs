e71c0aaed2fa1bdc48a307f924a56f3387439ba9
Peter Murphy <26548438+pete-murphy@users.noreply.github.com>
2025-03-17 00:27:06 -0400

    feat(S4): "name capture" problem

--- src/Session4.hs
+++ src/Session4.hs
@@ -3,6 +3,8 @@
 
 module Session4 where
 
+import Data.Set (Set)
+import Data.Set qualified as Set
 import Data.Text (Text)
 
 type Name = Text
@@ -23,6 +25,10 @@ example =
 
 -- >>> inlineSmall 100 example
 -- Add (Add (LInt 1) (LInt 1)) (Add (LInt 7) (LInt 7))
+-- >>> inlineSmall 0 example
+-- Let "x" (Add (LInt 1) (LInt 1)) (Let "y" (Var "x") (Let "x" (Add (LInt 7) (LInt 7)) (Add (Var "y") (Var "x"))))
+-- >>> inlineSmall 2 example
+-- Let "x" (Add (LInt 1) (LInt 1)) (Let "___" (Add (LInt 7) (LInt 7)) (Add (Var "x") (Var "___")))
 
 size :: Expr -> Int
 size (LInt _) = 1
@@ -43,14 +49,31 @@ inlineSmall t (Let n e1 e2) =
         else Let n e1' e2'
 
 subst :: Name -> Expr -> Expr -> Expr
-subst _ _ (LInt i) = (LInt i)
+subst _ _ (LInt i) = LInt i
 subst n e (Add e1 e2) = Add (subst n e e1) (subst n e e2)
 subst n e (Var n')
   | n == n' = e
   | otherwise = Var n'
 subst n e (Let n' e1 e2)
-  -- Leave e2 alone, because references to n will be to a different n
   | n == n' = Let n' e1' e2
+  | n' `Set.member` free e = Let n'' e1' (subst n e (subst n' (Var n'') e2))
   | otherwise = Let n' e1 (subst n e e2)
   where
     e1' = subst n e e1
+    n'' = "___"
+
+-- Get the free variables in an expression
+free :: Expr -> Set Name
+free (LInt _) = Set.empty
+free (Add e1 e2) = free e1 <> free e2
+free (Var n) = Set.singleton n
+free (Let n e1 e2) = free e1 <> Set.delete n (free e2)
+
+-- >>> free example
+-- fromList []
+
+example2 :: Expr
+example2 = Let "x" (Var "y") example
+
+-- >>> free example2
+-- fromList ["y"]
